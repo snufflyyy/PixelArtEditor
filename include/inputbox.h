a08b95bd7580770c69eb9ca20ce271b302d67d0d
snufflyyy <snufflyy@proton.me>
2024-07-24 10:50:42 -0500

    stuff

--- include/inputbox.h
+++ include/inputbox.h
@@ -8,3 +8,9 @@ typedef struct {
     char text[4]; // it is 4 bc of null terminator
     int numberCount;
 } ColorInputBox;
+
+ColorInputBox createColorInputBox();
+
+void getColorInputBoxInput(ColorInputBox* inputBox);
+
+void renderColorInputBox(ColorInputBox inputBox, Font font, int fontSize, Color buttonColor, Color textColor);
--- src/editor.c
+++ src/editor.c
@@ -20,7 +20,7 @@ float canvasBackgroundRoundness;
 float colorPickerBackgroundRoundness; // this one is also used for the frames background
 
 // now calculated based on screen size :)
-float padding; // change this name!!
+float padding;
 
 int numberOfFrames;
 int canvasSize;
@@ -34,12 +34,68 @@ float pixelSize;
 
 Rectangle colorPickerPreview;
 
-// current color
 Color currentColor = WHITE;
+
+Font robotoFont;
+
+ColorInputBox *selectedColorInputBox;
 
 ColorInputBox redInputBox;
 ColorInputBox greenInputBox;
 ColorInputBox blueInputBox;
+float colorInputBoxFontSize;
+
+Rectangle favoriteColorButton;
+Texture2D favoriteIcon;
+
+Rectangle applyColorButton;
+Texture2D applyColorIcon;
+
+Rectangle colorPickerSeparatorLine;
+
+void createFrame() {
+    numberOfFrames++;
+    canvasPixels = realloc(canvasPixels, numberOfFrames * sizeof(Pixel**));
+
+    canvasPixels[numberOfFrames - 1] = malloc(canvasSize * sizeof(Pixel*));
+    for (int x = 0; x < canvasSize; x++) {
+        canvasPixels[numberOfFrames - 1][x] = malloc(canvasSize * sizeof(Pixel));
+    }
+
+    // false = white | true = gray
+    bool color = false;
+    for (int x = 0; x < canvasSize; x++) {
+        for (int y = 0; y < canvasSize; y++) {
+            if (color) {
+                canvasPixels[numberOfFrames - 1][x][y].color = LIGHTGRAY;
+            } else {
+                canvasPixels[numberOfFrames - 1][x][y].color = WHITE;
+            }
+
+            color = !color;
+        }
+        color = !color;
+    }
+}
+
+void createEditor(int size) {
+    canvasSize = size;
+
+    robotoFont = LoadFont("assets/fonts/roboto/Roboto-Bold.ttf");
+
+    // color input boxes
+    redInputBox = createColorInputBox();
+    greenInputBox = createColorInputBox();
+    blueInputBox = createColorInputBox();
+
+    // textures
+    addFramePlusIcon = LoadTexture("assets/icons/plus.png");
+    favoriteIcon = LoadTexture("assets/icons/star.png");
+    applyColorIcon = LoadTexture("assets/icons/check.png");
+
+    createFrame();
+    resizeEditor();
+}
 
 void resizeEditor() {
     // padding
@@ -78,7 +134,6 @@ void resizeEditor() {
     // canvas
     pixelSize = (GetScreenHeight() - padding * 6) / canvasSize;
 
-    // god came down and told me how to do this
     float canvasPixelsXStartingPos = (float) GetScreenWidth() / 2 - (GetScreenHeight() - padding * 6) / 2;
     float canvasPixelsYStartingPos = (float) GetScreenHeight() / 2 - (GetScreenHeight() - padding * 6) / 2;
 
@@ -95,7 +150,10 @@ void resizeEditor() {
     colorPickerPreview.x = colorPickerBackgroundPanel.x + padding * 2;
     colorPickerPreview.y = colorPickerBackgroundPanel.y + padding * 2;
 
-    // color input boxes (NOT a fan of how i layed this out but its fine)
+    // color input boxes font size
+    colorInputBoxFontSize = (float) (colorPickerBackgroundPanel.width + colorPickerBackgroundPanel.height) / 40;
+
+    // color input boxes
     redInputBox.box.width = (colorPickerPreview.width - padding * 2) / 3;
     redInputBox.box.height = colorPickerPreview.height / 8; // magic bullshit
     redInputBox.box.x = colorPickerPreview.x;
@@ -111,6 +169,21 @@ void resizeEditor() {
     blueInputBox.box.x = colorPickerPreview.x + greenInputBox.box.width * 2 + padding * 2;
     blueInputBox.box.y = colorPickerPreview.y + colorPickerPreview.height + padding * 2;
 
+    favoriteColorButton.width = (colorPickerPreview.width / 2) - padding / 2;
+    favoriteColorButton.height = colorPickerBackgroundPanel.height / 35;
+    favoriteColorButton.x = redInputBox.box.x;
+    favoriteColorButton.y = redInputBox.box.y + redInputBox.box.height + padding * 2;
+
+    applyColorButton.width = (colorPickerPreview.width / 2) - padding / 2;
+    applyColorButton.height = colorPickerBackgroundPanel.height / 35;
+    applyColorButton.x = favoriteColorButton.x + favoriteColorButton.width + padding;
+    applyColorButton.y = favoriteColorButton.y;
+
+    colorPickerSeparatorLine.width = colorPickerPreview.width;
+    colorPickerSeparatorLine.height = colorPickerBackgroundPanel.height / 200;
+    colorPickerSeparatorLine.x = favoriteColorButton.x;
+    colorPickerSeparatorLine.y = favoriteColorButton.y + favoriteColorButton.height + padding * 2;
+
     // add frame button
     addFrameButton.width = framesBackgroundPanel.width - padding * 4;
     addFrameButton.height = framesBackgroundPanel.height / 35; // magic bullshit
@@ -122,117 +195,9 @@ void resizeEditor() {
     addFramePlusIcon.height = addFramePlusIcon.width;
 }
 
-void createFrame() {
-    numberOfFrames++;
-    canvasPixels = realloc(canvasPixels, numberOfFrames * sizeof(Pixel**));
-
-    canvasPixels[numberOfFrames - 1] = malloc(canvasSize * sizeof(Pixel*));
-    for (int x = 0; x < canvasSize; x++) {
-        canvasPixels[numberOfFrames - 1][x] = malloc(canvasSize * sizeof(Pixel));
-    }
-
-    // create checkerboard pattern
-
-    // false = white | true = gray
-    bool color = false;
-    for (int x = 0; x < canvasSize; x++) {
-        for (int y = 0; y < canvasSize; y++) {
-            if (color) {
-                canvasPixels[numberOfFrames - 1][x][y].color = LIGHTGRAY;
-            } else {
-                canvasPixels[numberOfFrames - 1][x][y].color = WHITE;
-            }
-
-            color = !color;
-        }
-        color = !color;
-    }
-}
-
-void createEditor(int size) {
-    canvasSize = size;
-
-    // color input boxes
-    redInputBox.text[0] = '2';
-    redInputBox.text[1] = '5';
-    redInputBox.text[2] = '5';
-    redInputBox.text[3] = '\0';
-    redInputBox.numberCount = 3;
-
-    greenInputBox.text[0] = '2';
-    greenInputBox.text[1] = '5';
-    greenInputBox.text[2] = '5';
-    greenInputBox.text[3] = '\0';
-    greenInputBox.numberCount = 3;
-
-    blueInputBox.text[0] = '2';
-    blueInputBox.text[1] = '5';
-    blueInputBox.text[2] = '5';
-    blueInputBox.text[3] = '\0';
-    blueInputBox.numberCount = 3;
-
-    // textures
-    addFramePlusIcon = LoadTexture("assets/plus.png");
-
-    createFrame();
-    resizeEditor();
-}
-
 void updateEditor() {
-    if (redInputBox.isActive || greenInputBox.isActive || blueInputBox.isActive) {
-        ColorInputBox *inputBox;
-
-        if (redInputBox.isActive) {
-            inputBox = &redInputBox;
-        }
-        if (greenInputBox.isActive) {
-            inputBox = &greenInputBox;
-        }
-        if (blueInputBox.isActive) {
-            inputBox = &blueInputBox;
-        }
-
-        SetMouseCursor(MOUSE_CURSOR_IBEAM);
-
-        int key = GetCharPressed();
-        while (key > 0) {
-            // only allow numbers
-            if ((key >= 48) && (key <= 57)) {
-                if (inputBox->isActive && inputBox->numberCount <= 3) {
-                    inputBox->text[inputBox->numberCount] = (char) key;
-                    inputBox->text[inputBox->numberCount + 1] = '\0';
-                    inputBox->numberCount++;
-                }
-            }
-
-            key = GetCharPressed();
-        }
-
-        if (IsKeyPressed(KEY_BACKSPACE)) {
-            if (inputBox->isActive) {
-                inputBox->numberCount--;
-                if (inputBox->numberCount < 0) {
-                    inputBox->numberCount = 0;
-                }
-                inputBox->text[inputBox->numberCount] = '\0';
-            }
-        }
-    } else {
-        SetMouseCursor(MOUSE_CURSOR_DEFAULT);
-
-        int redColor = atoi(redInputBox.text);
-        int greenColor = atoi(greenInputBox.text);
-        int blueColor = atoi(blueInputBox.text);
-
-        if (redColor <= 255) {
-            currentColor.r = redColor;
-        }
-        if (greenColor <= 255) {
-            currentColor.g = greenColor;
-        }
-        if (blueColor <= 255) {
-            currentColor.b = blueColor;
-        }
+    if (selectedColorInputBox != NULL) {
+        getColorInputBoxInput(selectedColorInputBox);
     }
 
     // frames panel & color picker panel
@@ -244,21 +209,44 @@ void updateEditor() {
         if (CheckCollisionPointRec(GetMousePosition(), framesBackgroundPanel)) {
             if (CheckCollisionPointRec(GetMousePosition(), addFrameButton)) {
                 createFrame();
-                printf("number of frames: %d\n", numberOfFrames);
             }
         }
 
         if (CheckCollisionPointRec(GetMousePosition(), colorPickerBackgroundPanel)) {
             if (CheckCollisionPointRec(GetMousePosition(), redInputBox.box)) {
                 redInputBox.isActive = true;
-            }
-            if (CheckCollisionPointRec(GetMousePosition(), greenInputBox.box)) {
+            } else if (CheckCollisionPointRec(GetMousePosition(), greenInputBox.box)) {
                 greenInputBox.isActive = true;
-            }
-            if (CheckCollisionPointRec(GetMousePosition(), blueInputBox.box)) {
+            } else if (CheckCollisionPointRec(GetMousePosition(), blueInputBox.box)) {
                 blueInputBox.isActive = true;
+            } else {
+                SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+
+                int redColor = atoi(redInputBox.text);
+                int greenColor = atoi(greenInputBox.text);
+                int blueColor = atoi(blueInputBox.text);
+
+                if (redColor <= 255) {
+                    currentColor.r = redColor;
+                }
+                if (greenColor <= 255) {
+                    currentColor.g = greenColor;
+                }
+                if (blueColor <= 255) {
+                    currentColor.b = blueColor;
+                }
             }
         }
+    }
+
+    if (redInputBox.isActive) {
+        selectedColorInputBox = &redInputBox;
+    } else if (greenInputBox.isActive) {
+        selectedColorInputBox = &greenInputBox;
+    } else if (blueInputBox.isActive) {
+        selectedColorInputBox = &blueInputBox;
+    } else {
+        selectedColorInputBox = NULL;
     }
 
     // canvas panel
@@ -287,26 +275,18 @@ void renderEditor() {
     // magic number (fix later!)
     DrawRectangleRounded(colorPickerPreview, 0.1f, 10, currentColor);
 
-    DrawRectangleRounded(redInputBox.box, 1, 10, (Color) {100, 100, 100, 50});
-    DrawRectangleRounded(greenInputBox.box, 1, 10, (Color) {100, 100, 100, 50});
-    DrawRectangleRounded(blueInputBox.box, 1, 10, (Color) {100, 100, 100, 50});
+    renderColorInputBox(redInputBox, robotoFont, colorInputBoxFontSize, (Color) {100, 100, 100, 50}, (Color) {100, 100, 100, 150});
+    renderColorInputBox(greenInputBox, robotoFont, colorInputBoxFontSize, (Color) {100, 100, 100, 50}, (Color) {100, 100, 100, 150});
+    renderColorInputBox(blueInputBox, robotoFont, colorInputBoxFontSize, (Color) {100, 100, 100, 50}, (Color) {100, 100, 100, 150});
 
-    if (redInputBox.isActive) {
-        DrawRectangleRoundedLinesEx(redInputBox.box, 1, 10, 2, (Color) {100, 100, 100, 150});
-    }
-    if (greenInputBox.isActive) {
-        DrawRectangleRoundedLinesEx(greenInputBox.box, 1, 10, 2, (Color) {100, 100, 100, 150});
-    }
-    if (blueInputBox.isActive) {
-        DrawRectangleRoundedLinesEx(blueInputBox.box, 1, 10, 2, (Color) {100, 100, 100, 150});
+    if (selectedColorInputBox != NULL) {
+        DrawRectangleRoundedLinesEx(selectedColorInputBox->box, 1, 10, 2, (Color) {100, 100, 100, 150});
     }
 
-    // temp until i find a nice font
-    DrawText(redInputBox.text, (redInputBox.box.x + redInputBox.box.width / 2) - ((float) MeasureText(redInputBox.text, 25) / 2), redInputBox.box.y + 3, 25, (Color) {100, 100, 100, 150});
+    DrawRectangleRounded(favoriteColorButton, 1, 10, (Color) {100, 100, 100, 50});
+    DrawRectangleRounded(applyColorButton, 1, 10, (Color) {100, 100, 100, 50});
 
-    DrawText(greenInputBox.text, (greenInputBox.box.x + greenInputBox.box.width / 2) - ((float) MeasureText(greenInputBox.text, 25) / 2), greenInputBox.box.y + 3, 25, (Color) {100, 100, 100, 150});
-
-    DrawText(blueInputBox.text, (blueInputBox.box.x + blueInputBox.box.width / 2) - ((float) MeasureText(blueInputBox.text, 25) / 2), blueInputBox.box.y + 3, 25, (Color) {100, 100, 100, 150});
+    DrawRectangleRounded(colorPickerSeparatorLine, 1, 10, (Color) {100, 100, 100, 50});
 
     // add frame button
     DrawRectangleRounded(addFrameButton, 1, 10, (Color) {100, 100, 100, 50});
@@ -315,12 +295,14 @@ void renderEditor() {
     // canvas
     for (int x = 0; x < canvasSize; x++) {
         for (int y = 0; y < canvasSize; y++) {
-            // why the fuck is the normal raylib draw rectangle not using floats for coords
             DrawRectangleV(canvasPixels[currentFrame][x][y].position, (Vector2) {pixelSize, pixelSize}, canvasPixels[currentFrame][x][y].color);
         }
     }
 }
 
 void destoryEditor() {
+    canvasPixels = NULL;
+    selectedColorInputBox = NULL;
+
     UnloadTexture(addFramePlusIcon);
 }
